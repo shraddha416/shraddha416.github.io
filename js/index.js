d1f548312f1907d5f3f98442d31b7cfd15750985
shraddha416 <shraddhasuman416@gmail.com>
2016-01-10 12:31:34 +0530

    change-icons

--- js/index.js
+++ js/index.js
@@ -1,13 +1,5 @@
  $(function() {
     $("img.lazy").lazyload();
    //back to top
-     var $backToTop = $('#back-to-top').fadeIn();
-     $(window).scroll(function () {
-      if ($(this).scrollTop() > 600) {
-          $backToTop.fadeIn();
-      } else {
-          $backToTop.fadeOut();
-      }
-    });
-  });
+});
 
