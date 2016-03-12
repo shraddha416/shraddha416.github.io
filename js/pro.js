890aee3c26b538acb8805d2fae5d2000203b5a7b
shraddha416 <shraddhasuman416@gmail.com>
2016-03-13 02:19:17 +0530

    few changes
    change alignment in pricing page
    add content in procedure.html
    improve contact.html

--- js/pro.js
+++ js/pro.js
@@ -3,5 +3,19 @@ function myFunction1() {
   };
 function myFunction2() {
     var clipboard = new Clipboard('.btn2');
-
+};
+function myFunction3() {
+    var clipboard = new Clipboard('.btn3');
+};
+function myFunction4() {
+    var clipboard = new Clipboard('.btn4');
+};
+function myFunction5() {
+    var clipboard = new Clipboard('.btn5');
+};
+function myFunction6() {
+    var clipboard = new Clipboard('.btn6');
+};
+function myFunction7() {
+    var clipboard = new Clipboard('.btn7');
 };
